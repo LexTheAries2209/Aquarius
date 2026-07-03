948483c58a80ece43a0eb7bcc8be399e57ba0cf4
LexTheAries2209 <673661658@qq.com>
2026-07-03 19:00:25 +0800

    Initial Commit

---
+++ OCRTimecode/OCRTimecodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRTimecodeApp.swift
+//  OCRTimecode
+//
+//  Created by 吴坤城 on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct OCRTimecodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
